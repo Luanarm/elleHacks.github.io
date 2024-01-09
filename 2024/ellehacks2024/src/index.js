3ac3b5b8ea236fe738079cc2a21df6e33f78b9a4
Luana Madeira <105006518+Luanarm@users.noreply.github.com>
2024-01-09 11:28:05 -0500

    Add files via upload

--- 2024/ellehacks2024/src/index.js
+++ 2024/ellehacks2024/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App'; // adjust the path if necessary
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
