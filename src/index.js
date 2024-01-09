72c37c9408ef552fcc8e016c4ade4c709f2fa54d
Luanarm <luanarmadeira.lrm@gmail.com>
2024-01-09 12:58:29 -0500

    testing gh pages

--- src/index.js
+++ src/index.js
@@ -9,15 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
->>>>>>> 02a9e0c5fafedfc6be5ee7fb33d7604d8f8b589f
