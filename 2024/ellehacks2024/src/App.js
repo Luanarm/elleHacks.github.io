29ab8836b8b329977f8132e7cc69773dbcdf0968
Luana Madeira <105006518+Luanarm@users.noreply.github.com>
2024-01-09 03:22:24 -0500

    Add files via upload

--- 2024/ellehacks2024/src/App.js
+++ 2024/ellehacks2024/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import JoinUS from './components/JoinUS';
+import FAQ from './components/FAQ';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Home id="home" />
+        <AboutUs id="aboutus" />
+        <JoinUS id="joinus" />
+        <FAQ id="faq" />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
