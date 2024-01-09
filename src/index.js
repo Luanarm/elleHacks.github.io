ea11d3a52af8de825611b0c80d59a8db25d416e5
Luanarm <luanarmadeira.lrm@gmail.com>
2024-01-09 16:06:53 -0500

    testing gh pages deploy

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'; // adjust the path if necessary
 
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
